f98e4ea08f6b30f4e91715e4f924afb984634ceb
TLucasandrade <Lucasandrade07e@gmail.com>
2026-05-21 18:18:40 -0300

    Overlay nas páginas do home-page e alteração da diretiva

--- src/app/directives/highlight.ts
+++ src/app/directives/highlight.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appMovieHighlight]',
+  standalone: true 
 })
-export class Highlight {
+export class MovieHighlightDirective {
+
+  @HostBinding('class.is-hovered') isHovered: boolean = false;
 
   constructor() { }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.isHovered = false;
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -14,6 +14,7 @@ import { RecentlyWatchedService } from '../../services/recently-watched.service'
 import { FavoritesService } from '../../services/favorites.service';
 import { LazyLoadDirective } from '../../directives/lazy-load';
 import { register } from 'swiper/element/bundle';
+import { MovieHighlightDirective } from '../../directives/highlight';
 register(); 
 
 @Component({
@@ -24,7 +25,7 @@ register();
   schemas: [CUSTOM_ELEMENTS_SCHEMA], 
   imports: [
     CommonModule, RouterModule, RouterLink, FormsModule,
-    IonContent, IonIcon, IonButton, LazyLoadDirective
+    IonContent, IonIcon, IonButton, LazyLoadDirective,MovieHighlightDirective
   ]
 })
 export class HomePage implements OnInit, AfterViewInit {
